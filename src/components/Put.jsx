8ac2d7dcd0d0ca4cad3bf5b66dd29b04b0b346dc
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-11 22:00:11 +0200

    add input delete to put

--- src/components/Put.jsx
+++ src/components/Put.jsx
@@ -2,6 +2,12 @@
 const Put = () => {
   return (
     <div>
+      <input
+        type="text"
+        placeholder="id"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
     </div>
   );
 }
