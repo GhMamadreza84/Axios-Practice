84714042a156d5e8bd5b882184e33e199c2dc0fc
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-11 22:07:30 +0200

    edit error

--- src/components/Put.jsx
+++ src/components/Put.jsx
@@ -6,7 +6,7 @@ const Put = () => {
   const [title, setTitle] = useState("");
   const updateHandler = () => {
     axios
-      .patch(`shttps://jsonplaceholder.typicode.com/posts/${id}`, { title })
+      .patch(`https://jssonplaceholder.typicode.com/posts/${id}`, { title })
       .then((res) => console.log(res))
       .catch((error) => console.log(error));
   };
