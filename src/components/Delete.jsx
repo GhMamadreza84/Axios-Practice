5aa2e9846275487d1e894fc5b718ca2aeb0a2287
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-11 21:49:15 +0200

    create state

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 
 const Delete = () => {
+  const [id, setId] = useState("");
   return (
     <div>
+      <input type="text" placeholder="id" />
     </div>
   );
-}
+};
 
 export default Delete;
